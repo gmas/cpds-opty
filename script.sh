4dc4cdd9cebcf89e5dee45fbb3fd8dc20b866015
George Masgras <gmasgras@gmail.com>
2023-12-12 14:47:34 +0100

    update script with mean, stdev, concurrent_clients functions

--- script.sh
+++ script.sh
@@ -1,20 +1,39 @@
 #!/bin/bash
+result_dir=results
 sleeptime=5
+
+rm -rf "./$result_dir"
+mkdir -p "./$result_dir"
 erl -make
+
+
+calc_mean() {
+  lines=$1
+  mean_r=$(echo "$lines" | awk '{ total += $1; count++ } END { print total/count }')
+  echo "$mean_r"
+}
+
+calc_stdev() {
+  lines=$1
+  stdev_r=$(echo "$lines" | awk '{sum+=$1; sumsq+=$1*$1}END{print sqrt(sumsq/NR - (sum/NR)**2)}')
+  echo "$stdev_r"
+}
 
 concurrent_clients() {
   maxtime=4
-  num_clients=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23)
-  d_entries=10
-  d_reads=1
-  d_writes=1
-  for clients in "${num_clients[@]}"; do
-    echo "$clients, $d_entries, $d_reads, $d_writes";
-    filename=opty"$clients".out;
-    erl -noshell -pa ebin -eval "opty:start($clients, $d_entries, $d_reads, $d_writes, $maxtime)" > numclients/$filename & pid=$!; sleep $sleeptime; kill $pid
-    geomean=$(grep "Mean" numclients/$filename | awk -F '[:]' '{print $2}')
-    stddev=$(grep "Stddev" numclients/$filename | awk -F '[:]' '{print $2}')
-    echo $clients, $geomean, $stddev >> numclients/clean
+  nr_clients=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20)
+  entries=10
+  reads=1
+  writes=1
+  echo "clients, mean, stdev" >> $result_dir/all.csv
+  for clients in "${nr_clients[@]}"; do
+    echo "$clients, $entries, $reads, $writes";
+    filename="$clients.out";
+    erl -noshell -pa ebin -eval "opty:start($clients, $entries, $reads, $writes, $maxtime)" > $result_dir/$filename & pid=$!; sleep $sleeptime; kill $pid
+    results=$(cat $result_dir/$filename | grep TOTAL | cut -d '>' -f2 | cut -d '%' -f1 | tr -d " ")
+    mean=$(calc_mean "$results")
+    stdev=$(calc_stdev "$results")
+    echo $clients, $mean, $stdev >> $result_dir/all.csv
   done
 }
 
