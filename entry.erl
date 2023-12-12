3bea410a34545395b67644d8c37cb6731bc8b8a2
George Masgras <gmasgras@gmail.com>
2023-12-12 11:43:04 +0100

    add Opty code

---
+++ entry.erl
@@ -0,0 +1,29 @@
+-module(entry).
+-export([new/1]).
+
+new(Value) ->
+    spawn_link(fun() -> init(Value) end).
+
+init(Value) ->
+    entry(Value, make_ref()).
+
+entry(Value, Time) ->
+    receive
+        {read, Ref, From} ->
+            %% TODO: ADD SOME CODE
+            From ! {Ref, self(), Value, Time},
+            entry(Value, Time);
+        {write, New} ->
+            entry(New, make_ref());  %% TODO: COMPLETE
+        {check, Ref, Readtime, From} ->
+            if
+                 Time == Readtime ->   %% TODO: COMPLETE
+                    %% TODO: ADD SOME CODE
+                    From ! {Ref, ok};
+                true ->
+                    From ! {Ref, abort}
+            end,
+            entry(Value, Time);
+        stop ->
+            ok
+    end.
