733b46680e902a1a740e04b8626c72f65119c366
George Masgras <gmasgras@gmail.com>
2023-12-18 22:17:44 +0100

    remove TODO comments

--- forward_validation/handler.erl
+++ forward_validation/handler.erl
@@ -10,29 +10,23 @@ init(Client, Validator, Store) ->
 handler(Client, Validator, Store, Reads, Writes) ->
     receive
         {read, Ref, N} ->
-            case lists:keyfind(N, 1, Writes) of  %% TODO: COMPLETE
+            case lists:keyfind(N, 1, Writes) of
                 {N, _, Value} ->
-                    %% TODO: ADD SOME CODE
                     Client ! {value, Ref, Value},
                     handler(Client, Validator, Store, Reads, Writes);
                 false ->
-                    %% TODO: ADD SOME CODE
-                    %% TODO: ADD SOME CODE
                     Entry = store:lookup(N, Store),
                     Entry ! {read, Ref, self()},
                     handler(Client, Validator, Store, Reads, Writes)
             end;
         {Ref, Entry, Value, Time} ->
-            %% TODO: ADD SOME CODE HERE AND COMPLETE NEXT LINE
             Client ! {value, Ref, Value},
             handler(Client, Validator, Store, [{Entry, Time}|Reads], Writes);
         {write, N, Value} ->
-            %% TODO: ADD SOME CODE HERE AND COMPLETE NEXT LINE
             Entry = store:lookup(N, Store),
             Added = lists:keystore(N, 1, Writes, {N, Entry, Value}),
             handler(Client, Validator, Store, Reads, Added);
         {commit, Ref} ->
-            %% TODO: ADD SOME CODE
             Validator ! {validate, Ref, Reads, Writes, Client};
         abort ->
             ok
